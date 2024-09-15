d8ed54f313fb8007bb614be4ba32d99c086e3ccd
nico60s <nico_60s@hotmail.com>
2024-09-15 16:11:47 -0300

    wip

---
+++ MedicalAppointment/MedicalAppointment.Modules.Auth/Application/Contracts/ClaimTypes.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MedicalAppointment.Modules.Auth.Application.Contracts
+{
+    public class ClaimTypes
+    {
+        public const string Roles = "roles";
+        public const string Email = "email";
+    }
+}
---
+++ MedicalAppointment/MedicalAppointment.Modules.Auth/Application/Contracts/IAuthModule.cs
@@ -0,0 +1,11 @@
+﻿
+using MedicalAppointment.Shared.Application.Contracts;
+
+namespace MedicalAppointment.Modules.Auth.Application.Contracts
+{
+    public interface IAuthModule
+    {
+        Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command);
+        Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query);
+    }
+}
