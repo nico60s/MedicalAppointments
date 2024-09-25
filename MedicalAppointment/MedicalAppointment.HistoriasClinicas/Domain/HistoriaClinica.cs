eda4da435c8af8ea00c24f2473e40a21cb82bb97
nico60s <nico_60s@hotmail.com>
2024-09-25 20:48:56 -0300

    chore

---
+++ MedicalAppointment/MedicalAppointment.HistoriasClinicas/Domain/HistoriaClinica.cs
@@ -0,0 +1,10 @@
+﻿
+
+using MedicalAppointment.Shared.Domain;
+
+namespace MedicalAppointment.HistoriasClinicas.Domain
+{
+    public class HistoriaClinica : Entity
+    {
+    }
+}
---
+++ MedicalAppointment/MedicalAppointment.HistoriasClinicas/HistoriaClinicaId.cs
@@ -0,0 +1,18 @@
+﻿
+
+using MedicalAppointment.Shared.Domain;
+
+namespace MedicalAppointment.HistoriasClinicas
+{
+    public class HistoriaClinicaId : ValueObject
+    {
+        public Guid Id { get; }
+        private HistoriaClinicaId(Guid id) => Id = id;
+        public static HistoriaClinicaId Create()=> new(Guid.NewGuid());
+        public static HistoriaClinicaId FromGuid(Guid id) => new(id);   
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
