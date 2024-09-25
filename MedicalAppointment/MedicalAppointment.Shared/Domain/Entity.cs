7332b6088d26c6879c6ec77ca22b8cef086dc1bc
nico60s <nico_60s@hotmail.com>
2024-09-25 20:49:09 -0300

    chore

--- MedicalAppointment/MedicalAppointment.Shared/Domain/Entity.cs
+++ MedicalAppointment/MedicalAppointment.Shared/Domain/Entity.cs
@@ -5,7 +5,6 @@ namespace MedicalAppointment.Shared.Domain
     public abstract class Entity<T> where T : ValueObject
     {
         protected T Id { get; set; }
-        public abstract IReadOnlyCollection<IIntegrationEvent> Events { get; }
         protected Entity(T id) => Id = id;
         protected void CheckRule(IBusinessRule rule)
         {
